35eda51e66b0adadeca6b82fdff368081cfea9bb
EGOIST <0x142857@gmail.com>
2017-03-18 01:37:12 +0800

    💯

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default function (Vue) {
-  Vue.prototype.setState = function (obj) {
+  Vue.prototype.setState = function (obj, cb) {
     const newState = typeof obj === 'function' ?
       obj(this.state, this.props) :
       obj
@@ -7,6 +7,8 @@ export default function (Vue) {
     for (const key in newState) { // eslint-disable-line guard-for-in
       this[key] = newState[key]
     }
+
+    cb && this.$nextTick(cb)
   }
 
   Object.defineProperties(Vue.prototype, {
