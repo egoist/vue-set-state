306d66525790c87b05af10bb8583eef8f6e6484c
EGOIST <0x142857@gmail.com>
2017-03-21 01:49:29 +0800

    fix test finally

--- test/index.test.js
+++ test/index.test.js
@@ -4,16 +4,20 @@ import setState from '../src'
 Vue.use(setState)
 
 test('main', () => {
-  const vm = new Vue({
-    data: { count: 0 },
-    methods: {
-      inc() {
-        this.setState({ count: this.state.count + 1 })
+  return new Promise(resolve => {
+    const vm = new Vue({
+      data: { count: 0 },
+      methods: {
+        inc() {
+          this.setState({ count: this.state.count + 1 })
+        }
       }
-    }
+    })
+    vm.inc()
+    expect(vm.state.count).toBe(0)
+    vm.$nextTick(() => {
+      expect(vm.state.count).toBe(1),
+      resolve()
+    })
   })
-  vm.inc()
-  expect(vm.state.count).toBe(0)
-  vm._watcher.run()
-  expect(vm.state.count).toBe(1)
 })
